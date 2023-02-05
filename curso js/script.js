f958c046620d4192f5f19fde144278b7312066c2
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-05 21:07:39 +0100

    NumPrime

--- curso js/script.js
+++ curso js/script.js
@@ -1,14 +1,62 @@
 "use strict";
 
+let n = 10;
 
-let number;
+nextPrime:
+for (let i = 2; i <= n; i++) { // por cada i...
+
+  for (let j = 2; j < i; j++) { // buscar un divisor..
+    if (i % j == 0) continue nextPrime; // no es primo, ir al próximo i
+  }
+
+  alert( i ); // primo
+}
+
+/*
+let n = prompt("escribe un número",);
+let i = 2;
+
+do{
+  if (n % i === 0){
+    primo=false;
+  }
+  i++;
+} while (i < n);
+
+if ( n % i !=== 0){
+
+}
+
+
+
+/*
+
+let sinresto = 
+
+
+let k = n % i; null;
+
+for (let i = n; i >= 1){
+  if (k ==)
+}
+
+let resp = 1;
+
+
+
+
+
+
+
+
+/*let number;
 
 do{
   number = prompt("Ingresa un número mayor a 100","0");
   }
  while (number <= 100);
 
-
+*/
 /*
 for (let i = 0; i < 10; i++) {
 
