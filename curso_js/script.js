ed9b3f1e5fde804a5804ff5647de9cb3472968ba
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-07 18:28:07 +0100

    simpleCalculator_with_js

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,26 +1,53 @@
 
-let free = false;
+const sumar = (num1, num2) => {
+  return parseInt(num1) + parseInt(num2);
+}
 
-const validarCliente = (time) => {
-  let edad = prompt("Qué edad tienes?");
-  if (edad >= 18){
-    if(time >= 2 && time < 7 && free == false){
-      alert("Puedes pasar gratis")
-      free = true;
-    } else {
-      alert("Puedes pasar pero pagando la entrada.")
-    }
-  } 
+const restar = (num1, num2) => {
+  return parseInt(num1) - parseInt(num2);
+}
+
+const dividir = (num1, num2) => {
+  return parseInt(num1) / parseInt(num2);
+}
+const multiplicar = (num1, num2) => {
+  return parseInt(num1) * parseInt(num2);
+}
+
+
+
+
+alert("Qué operación deseas realizar?")
+let operacion = prompt("1: suma, 2: resta, 3: división, 4: multiplicación");
+
+if(operacion == 1){
+  let numero1 = prompt("primer numero para sumar");
+  let numero2 = prompt("segundo numero para sumar");
+  resultado = sumar(numero1,numero2);
+  alert(`El resultado es ${resultado}`);
+}
+
+else if ( operacion == 2){
+  let numero1 = prompt("primer numero para restar");
+  let numero2 = prompt("segundo numero para restar");
+  resultado = restar(numero1,numero2);
+  alert(`El resultado es ${resultado}`);
+}
+
+else if ( operacion == 3){
+  let numero1 = prompt("primer numero para dividir");
+  let numero2 = prompt("segundo numero para dividir");
+  resultado = dividir(numero1,numero2);
+  alert(`El resultado es ${resultado}`);
+}
+
+else if ( operacion == 4){
+  let numero1 = prompt("primer numero para multiplicar");
+  let numero2 = prompt("segundo numero para multiplicar");
+  resultado = multiplicar(numero1,numero2);
+  alert(`El resultado es ${resultado}`);
+}
+
 else {
-  alert("Lo siento, no puedes pasar.")
+  alert("No se ha encontrado la operación");
 }
-}
-
-
-
-validarCliente(23);
-validarCliente(24);
-validarCliente(0.50);
-validarCliente(1);
-validarCliente(2);
-validarCliente(3);
