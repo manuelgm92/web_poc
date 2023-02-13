15f45a1f85dbc3fd4c162d7293d3409aa79aa88e
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-13 20:09:46 +0100

    b.

---
+++ bootcamp/script.js
@@ -0,0 +1,23 @@
+console.log("Javascript");
+
+
+//let firstName = "Manuel"; //permitimos que nombre guarde la información "x"
+//const lastName = 4;  // constante, no se puede reasignar pero se puede añadir datos
+
+
+
+//Tipos primitivos/inmutables:
+// "string", 4, true/false, undefined, null
+//Tipos mutables:
+// array, funciones,... puedes cambiar su valor
+
+
+const list = [10]       //array
+//list.push(157) // .push se utiliza para añadir un valor al array
+const anotherList = list.concat([157,333]) // .concat sería concatenarlo creando un nuevo array con el array anterior, mejor utilizar concat
+
+
+console.log(anotherList);
+
+
+
