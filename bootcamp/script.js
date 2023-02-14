be97dd191aea663f718a3fc39ed5f6fd200f559b
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-14 18:45:54 +0100

    function

--- bootcamp/script.js
+++ bootcamp/script.js
@@ -1,23 +1,53 @@
-console.log("Javascript");
+
+const sumar = (a, b) => {
+    console.log(a);
+    console.log(b);
+    return a + b
+}
+
+const op1 = 8
+const op2 = 2
+
+console.log(sumar(op1, op2))
 
 
-//let firstName = "Manuel"; //permitimos que nombre guarde la información "x"
-//const lastName = 4;  // constante, no se puede reasignar pero se puede añadir datos
 
 
 
-//Tipos primitivos/inmutables:
-// "string", 4, true/false, undefined, null
-//Tipos mutables:
-// array, funciones,... puedes cambiar su valor
 
 
-const list = [10]       //array
-//list.push(157) // .push se utiliza para añadir un valor al array
-const anotherList = list.concat([157,333]) // .concat sería concatenarlo creando un nuevo array con el array anterior, mejor utilizar concat
+// console.log("Javascript");
 
 
-console.log(anotherList);
+// //let firstName = "Manuel"; //permitimos que nombre guarde la información "x"
+// //const lastName = 4;  // constante, no se puede reasignar pero se puede añadir datos
 
+
+
+// //Tipos primitivos/inmutables:
+// // "string", 4, true/false, undefined, null
+// //Tipos mutables:
+// // array, funciones,... puedes cambiar su valor
+
+
+// // const list = [10]       //array
+// // //list.push(157) // .push se utiliza para añadir un valor al array
+// // const anotherList = list.concat([157,333]) // .concat sería concatenarlo creando un nuevo array con el array anterior, mejor utilizar concat
+
+
+// // console.log(anotherList);
+
+
+// const persona = {
+//     name: "Manuel",
+//     twitter: "@manu",
+//     isDeveloper: true,
+//     age: 30,
+//     hobbies: ["a", "music"],
+// }
+
+// // const field = "twitter"
+
+// // console.log(persona[field]);
 
 
