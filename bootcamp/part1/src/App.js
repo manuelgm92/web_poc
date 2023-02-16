48f73d6b947b5001dc56c5195c58d11e24aa188c
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-16 19:07:54 +0100

    Practice1.react

--- bootcamp/part1/src/App.js
+++ bootcamp/part1/src/App.js
@@ -1,24 +1,31 @@
 
-import './App.css';
-import Mensaje from './Mensaje.js';
 
 
-const Description = () => {
-  return <p>Esta es la app del fullstack bootcamp</p>
-}
-
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Título de la App</h1>
-      <Mensaje color='green' message='Estamos trabajando '/>
-      <Mensaje color='grey' message='en un curso de React'/>
-      <Description></Description>
-    </div>
-  );
-}
-
-export default App;
 
 
-//los componentes deben ponerse con mayusculas y no debe haber un componente dentro de otro
+
+
+
+// import './App.css';
+// import Mensaje from './Mensaje.js';
+// document.write("hola")
+
+// const Description = () => {
+//   return <p>Esta es la app del fullstack bootcamp</p>
+// }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Título de la App</h1>
+//       <Mensaje color='green' message='Estamos trabajando '/>
+//       <Mensaje color='grey' message='en un curso de React'/>
+//       <Description></Description>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// //los componentes deben ponerse con mayusculas y no debe haber un componente dentro de otro
