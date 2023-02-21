5d81faad2f29b7fe9e1232c8cd6ac2cfe4d099f3
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-21 12:04:29 +0100

    for_if

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,7 +1,22 @@
+// for ( let i = "#"; i.length <= 7; i+= "#"){
+//     console.log(i);
+// }
+let i = 1;
+i++;
 
-for (let i = "#"; i.length <= 7; i+= "#" ) { 
+for (i = 1; i <= 100; i++) {
+    let output = "";    
     
-    console.log(i);
-
+    if ( i % 3 == 0 && i % 5 == 0){
+    output = "FizzBuzz"
+    }
+    if (i % 3 == 0 && i % 5 !== 0 ){
+       output = "Fizz"
+    } else if ( i % 5 == 0 && i % 3 !== 0){
+        output += "Buzz"
+    } 
+    
+    console.log(output || i);
+    
 }
 
