196f026a082f83dbecea2fe055211eecb875ee30
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-22 19:07:54 +0100

    switch_2

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,31 +1,27 @@
-function caseInSwitch(val) {
+function switchOfStuff(val) {
     let answer = "";
     switch(val){
-        case 1: 
-        return "alpha"
-        break;        
-        
-        case 2:
-        return "beta"
-        break;
-        
-        case 3:
-        return "gamma"
-        break;
-        
-        case 4:
-        return "delta"
-        break;
+        case "a": 
+            return "apple";
+    
+        case "b": 
+            return "bird";
+            break;
+
+        case "c": 
+            return "cat";
+            break;
+
+        default: 
+            return "stuff";
+            break;
     }
-    return answer;
-}
+  
 
-document.write(caseInSwitch(1));
-
-
-
-
-
+  return answer;
+  }
+  
+document.write(switchOfStuff("2"));
 
 
 
