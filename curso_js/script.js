26b65a5922c5e188314f5ef7ea0326d1e713f07c
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-22 18:52:21 +0100

    switch_exercise_freecodecamp

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,14 +1,64 @@
-let esPar = (n) => {
-    if (n === 0) { 
-        return true;
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val){
+        case 1: 
+        return "alpha"
+        break;        
+        
+        case 2:
+        return "beta"
+        break;
+        
+        case 3:
+        return "gamma"
+        break;
+        
+        case 4:
+        return "delta"
+        break;
     }
-    else if (n == 1) {
-        return false;
-    } else if (n < 0) {
-        return esPar(-n)
-    } else {
-        return esPar(n - 2)
-    }
+    return answer;
 }
 
-document.write(esPar(-2));
+document.write(caseInSwitch(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let esPar = (n) => {
+//     if (n === 0) { 
+//         return true;
+//     }
+//     else if (n == 1) {
+//         return false;
+//     } else if (n < 0) {
+//         return esPar(-n)
+//     } else {
+//         return esPar(n - 2)
+//     }
+// }
+
+// document.write(esPar(-2));
