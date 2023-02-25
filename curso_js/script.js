056c65a00e8cc7bb0db4df3c9bb8375ddb37c163
manuelgm1992 <manugonzalez1992@gmail.com>
2023-02-25 23:06:56 +0100

    exercise_object_js

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,19 +1,61 @@
-function checkObj(obj, checkProp) {
-   
-    if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  } else {
-    return "Not Found";
+// Configuración
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
+};
 
-  document.write()
-  
-  
-  
-  
+// Cambia solo el código debajo de esta línea
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== ""){
+    records[id][prop] = value;
+  } 
+  else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false){
+    records[id][prop] = [value];
+  }
+  else if (prop === "tracks" && value !== ""){
+    records[id][prop].push(value);
+  } 
+  else if (value === ""){
+    delete records[id][prop];
+  }
+    
+   return records;
 }
 
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
+
+
+
+
+
+
+
+// Cambia solo el código debajo de esta línea
+
+ // function updateRecords(records, id, prop, value) {
+ //   if
+
+
+ //   } 
+ //    return records;
+
+//document.write(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
 
 
 // let esPar = (n) => {
