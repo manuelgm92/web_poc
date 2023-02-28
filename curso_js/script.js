4d3d9783cb8213019dc35338f7571f80f62028a2
Manuel Gonzalez Martinez <manugonzalez1992@gmail.com>
2023-02-28 18:28:16 +0100

    function_with_for_i_j

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,15 +1,19 @@
-// Configuración
-const myArr = [2, 3, 4, 5, 6];
-
-// Cambia solo el código debajo de esta línea
-let total = 0;
-for (let i = 0; i < myArr.length; i++){
-  total += myArr[i];
-  console.log(total)
+function multiplyAll(arr) {
+  let product = 1;
+  // Cambia solo el código debajo de esta línea
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr[i].length; j++){
+      product = product * arr[i][j];
+    }
+  }  
+  return product;
 }
 
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
 
+
+document.write(multiplyAll([[1], [2], [3]]));
 
 
 
