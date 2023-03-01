3c0c27ae49b5e823e41f1a8f2815b2aa1516a179
Manuel Gonzalez Martinez <manugonzalez1992@gmail.com>
2023-03-01 18:02:44 +0100

    function_array_for_if

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,19 +1,51 @@
-function multiplyAll(arr) {
-  let product = 1;
+// Configuración
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
   // Cambia solo el código debajo de esta línea
-  for (let i = 0; i < arr.length; i++){
-    for (let j = 0; j < arr[i].length; j++){
-      product = product * arr[i][j];
+for(let i = 0; i < contacts.length;i++){
+    if (contacts[i].firstName === name){
+      if (prop in contacts[i]){
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
     }
-  }  
-  return product;
+}
+    return "No such contact";
+
+  // Cambia solo el código encima de esta línea
 }
 
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+document.write(lookUpProfile("Kristian", "lastName"));
 
 
 
-document.write(multiplyAll([[1], [2], [3]]));
 
 
 
