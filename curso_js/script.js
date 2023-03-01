d939630e2cf42d37986dac63ac328e40359dcb14
Manuel Gonzalez Martinez <manugonzalez1992@gmail.com>
2023-03-01 18:44:04 +0100

    m

--- curso_js/script.js
+++ curso_js/script.js
@@ -1,50 +1,11 @@
-// Configuración
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+function randomWholeNum() {
 
-function lookUpProfile(name, prop) {
   // Cambia solo el código debajo de esta línea
-for(let i = 0; i < contacts.length;i++){
-    if (contacts[i].firstName === name){
-      if (prop in contacts[i]){
-        return contacts[i][prop];
-      } else {
-        return "No such property";
-      }
-    }
-}
-    return "No such contact";
 
-  // Cambia solo el código encima de esta línea
+  return Math.random();
 }
 
-document.write(lookUpProfile("Kristian", "lastName"));
-
-
+document.write(randomWholeNum());
 
 
 
